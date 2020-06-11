645da99f00573fc2b3fa22ef45e62010b403809b
forecho <caizhenghai@gmail.com>
2020-06-11 11:06:15 +0800

    feat: 完成 Google v3 翻译

--- README.md
+++ README.md
@@ -36,27 +36,32 @@ Usage
 
 $config = [
     // HTTP 请求的超时时间（秒）
-    'timeout' => 5.0,
-    'from' => 'zh', // 源本语言，可选参数，默认是 zh
-    'to' => 'en', // 要翻译成的语言，可选参数，默认是 en
+	'timeout' => 5.0,
+	'from' => 'zh', // 源本语言，可选参数，默认是 zh
+	'to' => 'en', // 要翻译成的语言，可选参数，默认是 en
 
     // 可用的平台配置
-    'platforms' => [
-       'qqtranslation' => [
-            'app_id' => '你的 QQ AI 应用ID',
-            'app_key' => '你的 QQ AI 应用密钥',
-        ],
-       'baidu' => [
-            'app_id' => '百度翻译应用 ID',
-            'app_key' => '百度翻译应用秘钥',
-       ],
-       'google_v2' => [
-            'key' => 'xxx', // https://console.cloud.google.com/apis/credentials
-        ],
-       'caiyun' => [
-            'token' => 'xxx', // https://fanyi.caiyunapp.com/#/api
-       ],
-    ],
+	'platforms' => [
+		'qqtranslation' => [
+			'app_id' => '你的 QQ AI 应用ID',
+			'app_key' => '你的 QQ AI 应用密钥',
+		],
+		'baidu' => [
+			'app_id' => '百度翻译应用 ID',
+			'app_key' => '百度翻译应用秘钥',
+		],
+		'google_v2' => [
+			'key' => 'xxx', // https://console.cloud.google.com/apis/credentials
+		],
+		'google_v3' => [
+			'project_id' => 'xxxx', // Optional
+             // https://github.com/googleapis/google-cloud-php/blob/master/AUTHENTICATION.md
+			'key_json' => '{"type":"xx","project_id":"xx","private_key_id":"xx","private_key":"","client_email":"xx","client_id":"xx","auth_uri":"xx","token_uri":"xx","auth_provider_x509_cert_url":"xx","client_x509_cert_url":"xx"}',
+		],
+		'caiyun' => [
+			'token' => 'xxx', // https://fanyi.caiyunapp.com/#/api
+		],
+	],
 ];
 
 $translate = new \yiier\translate\Translate($config, 'qqtranslation');
@@ -68,9 +73,17 @@ $translate->setFrom('zh')->translate('今天天气怎么样');
 $translate->setFrom('zh')->setTo('de')->translate('今天天气怎么样');
 
 $translate->setTo('de')->translate('今天天气怎么样');
+
 ```
 
 如果使用 `setFrom` 或者 `setTo`，会忽略配置文件中的配置。
+
+
+⚠ 注意：如果要使用 `google_v3`，必须要安装官方依赖包：
+
+```
+composer require google/cloud-translate
+```
 
 
 PS：语言支持以 Google 的[《语言支持》](https://cloud.google.com/translate/docs/languages) 为标准。
@@ -79,4 +92,6 @@ PS：语言支持以 Google 的[《语言支持》](https://cloud.google.com/tra
 
 - [[译] 使用谷歌Cloud Translation API翻译文本](https://segmentfault.com/a/1190000014205232)
 - [Translate Text with the Translation API](https://codelabs.developers.google.com/codelabs/cloud-translation-intro/#0)
+- [为服务器到服务器的生产应用设置身份验证](https://cloud.google.com/docs/authentication/production)
+- [翻译文本（高级版）](https://cloud.google.com/translate/docs/advanced/translating-text-v3)
 
--- src/platforms/Googlev3Platform.php
+++ src/platforms/Googlev3Platform.php
@@ -9,6 +9,7 @@ namespace yiier\translate\platforms;
 
 use Google\Cloud\Translate\V3\TranslationServiceClient;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
 use yiier\translate\exceptions\TranslateException;
 
 /**
@@ -27,75 +28,42 @@ class Googlev3Platform extends Platform
     protected $endpoint;
 
     /**
-     * @return \GuzzleHttp\Client
+     * @return null
      */
     public function getClient()
     {
-        $headers = [
-            'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.130 Safari/537.36',
-        ];
-
-        $client = new \GuzzleHttp\Client([
-            'headers' => $headers,
-            'Authorization' => 'Bearer ' . $this->config->get('token'),
-            'timeout' => method_exists($this, 'getTimeout') ? $this->getTimeout() : 5.0,
-        ]);
-
-        $this->endpoint = $this->config->get('endpoint') ?: self::ENDPOINT;
-
-        return $client;
-    }
-
-
-    public function translate(string $string, string $from, string $to)
-    {
-        $projectId = $this->config->get('project_id');
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=/var/www/ft-erp/web/google.json');
-        $translationClient = new TranslationServiceClient([
-            'credentials' => getenv('GOOGLE_APPLICATION_CREDENTIALS')
-            // 'keyFile' => json_decode(file_get_contents('/path/to/keyfile.json'), true)
-        ]);
-        $content = [$string];
-        $response = $translationClient->translateText(
-            $content,
-            $to,
-            TranslationServiceClient::locationName($projectId, 'global')
-        );
-
-        foreach ($response->getTranslations() as $key => $translation) {
-            $separator = $key === 2
-                ? '!'
-                : ', ';
-            echo $translation->getTranslatedText() . $separator;
-        }
-        pr(1);
-
-        $params = [
-            'contents' => [$string],
-            'targetLanguageCode' => $to,
-            'sourceLanguageCode' => $from,
-            'model' => sprintf('projects/%s/locations/global/models/general/base', $projectId),
-        ];
-
-        $response = $this->client->post(sprintf($this->endpoint, $projectId), ['json' => $params]);
-        return $this->parseResult((string)$response->getBody());
+        return null;
     }
 
     /**
-     * Parse result
-     *
-     * @param string $result
+     * @param string $string
+     * @param string $from
+     * @param string $to
      * @return string
      * @throws TranslateException
      */
-    protected function parseResult($result)
+    public function translate(string $string, string $from, string $to)
     {
-        $rsp = json_decode($result);
-        $result = ArrayHelper::getValue($rsp, 'data.translations.0.translatedText', '');
+        $keyJson = $this->config->get('key_json');
+        $key = Json::decode($keyJson, true);
+        $projectId = $this->config->get('project_id') ?: ArrayHelper::getValue($key, 'project_id');
+        $content = [$string];
 
-        if ($result) {
-            return $result;
+        try {
+            $translationServiceClient = new TranslationServiceClient(
+                ['credentials' => $key]
+            );
+            $formattedParent = $translationServiceClient->locationName($projectId, 'global');
+            $response = $translationServiceClient->translateText(
+                $content,
+                $to,
+                $formattedParent
+            );
+            foreach ($response->getTranslations() as $translation) {
+                return $translation->getTranslatedText();
+            }
+        } catch (\Exception $e) {
+            throw new TranslateException('翻译返回结果失败' . $e->getMessage(), (array)$e, 400);
         }
-        throw new TranslateException('翻译返回结果失败', (array)$result, 400);
     }
 }
